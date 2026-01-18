1a04b3bb636cc64068d2bf2f9dffb72d1177c7ea
Brad Carter <16466430+carterbs@users.noreply.github.com>
2026-01-17 20:41:24 -0500

    Add checkmark icon to completed sets for clearer visual feedback

--- packages/client/src/components/Workout/SetRow.tsx
+++ packages/client/src/components/Workout/SetRow.tsx
@@ -1,6 +1,19 @@
 import { Box, Button, Flex, Text, Badge } from '@radix-ui/themes';
 import type { WorkoutSet, WorkoutStatus, LogWorkoutSetInput } from '@lifting/shared';
 
+function CheckIcon(): JSX.Element {
+  return (
+    <svg width="18" height="18" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M11.4669 3.72684C11.7558 3.91574 11.8369 4.30308 11.648 4.59198L7.39799 11.092C7.29783 11.2452 7.13556 11.3467 6.95402 11.3699C6.77247 11.3931 6.58989 11.3355 6.45446 11.2124L3.70446 8.71241C3.44905 8.48022 3.43023 8.08494 3.66242 7.82953C3.89461 7.57412 4.28989 7.55529 4.5453 7.78749L6.75292 9.79441L10.6018 3.90792C10.7907 3.61902 11.178 3.53795 11.4669 3.72684Z"
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
 interface SetRowProps {
   set: WorkoutSet;
   workoutStatus: WorkoutStatus;
@@ -54,12 +67,17 @@ export function SetRow({
       }}
       onClick={isDisabled ? undefined : onClick}
     >
-      {/* Set number */}
-      <Box style={{ minWidth: '40px' }}>
-        <Text size="2" weight="medium" color="gray">
+      {/* Set number with check icon */}
+      <Flex align="center" gap="2" style={{ minWidth: '60px' }}>
+        {isLogged && (
+          <Box style={{ color: 'var(--green-9)' }}>
+            <CheckIcon />
+          </Box>
+        )}
+        <Text size="2" weight="medium" color={isLogged ? undefined : 'gray'}>
           Set {set.set_number}
         </Text>
-      </Box>
+      </Flex>
 
       {/* Target values */}
       <Flex direction="column" gap="1" style={{ flex: 1 }}>
